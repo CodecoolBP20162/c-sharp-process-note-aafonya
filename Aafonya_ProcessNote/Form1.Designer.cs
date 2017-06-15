6a3292fc0eaa31f2dda408c18ab17df9d7d1a189
Judit <tolnai.judit12@gmail.com>
2017-06-15 23:38:21 +0200

    processnote

---
+++ Aafonya_ProcessNote/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Aafonya_ProcessNote
+{
+    partial class ProcessNote
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ShowProcesses = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // ShowProcesses
+            // 
+            this.ShowProcesses.BackColor = System.Drawing.Color.Chartreuse;
+            this.ShowProcesses.Location = new System.Drawing.Point(227, 12);
+            this.ShowProcesses.Name = "ShowProcesses";
+            this.ShowProcesses.Size = new System.Drawing.Size(94, 23);
+            this.ShowProcesses.TabIndex = 0;
+            this.ShowProcesses.Text = "ShowProcesses";
+            this.ShowProcesses.UseVisualStyleBackColor = false;
+            this.ShowProcesses.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(338, 12);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(40, 23);
+            this.Stop.TabIndex = 1;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(45, 72);
+            this.checkedListBox1.MultiColumn = true;
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(200, 139);
+            this.checkedListBox1.TabIndex = 3;
+            this.checkedListBox1.Visible = false;
+            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(158, 12);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(54, 23);
+            this.updateButton.TabIndex = 4;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(287, 72);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(204, 134);
+            this.listBox1.TabIndex = 5;
+            // 
+            // ProcessNote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(536, 261);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.ShowProcesses);
+            this.Name = "ProcessNote";
+            this.Text = "ProcessNote";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ShowProcesses;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Aafonya_ProcessNote/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Aafonya_ProcessNote
+{
+    public partial class ProcessNote : Form
+    {
+        Process[] process;
+
+        System.Windows.Forms.Timer t;
+
+        public ProcessNote()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t = new System.Windows.Forms.Timer();
+            t.Interval = 10000;
+            t.Tick += new EventHandler(t_Tick);
+            t.Start();
+
+            checkedListBox1.Visible = true;
+            process = Process.GetProcesses();
+
+            foreach (Process prs in process)
+            {
+                checkedListBox1.Items.Add(prs.ProcessName + "         (" + prs.PrivateMemorySize64.ToString() + ")");
+            }
+        }
+
+        void t_Tick(object sender, EventArgs e)
+        {
+            const string text = "Process list updated";
+            const string caption = "Update message";
+            MessageBoxButtons buttonsForMessageBox = MessageBoxButtons.OK;
+            MessageBox.Show(text, caption, buttonsForMessageBox);
+
+            foreach (Process prs in process)
+            {
+                checkedListBox1.Items.Remove(prs);
+            }
+
+            process = null;
+            process = Process.GetProcesses();
+
+            foreach (Process prs in process)
+            {
+                checkedListBox1.Items.Add(prs.ProcessName + "         (" + prs.PrivateMemorySize64.ToString() + ")");
+            }
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            t.Stop();
+
+            checkedListBox1.Visible = false;
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            const string text = "Process list updated";
+            const string caption = "Caption";
+            MessageBoxButtons buttonsForMessageBox = MessageBoxButtons.OK;
+            MessageBox.Show(text, caption, buttonsForMessageBox);
+
+            foreach (Process prs in process)
+            {
+                checkedListBox1.Items.Remove(prs);
+            }
+
+            process = null;
+            process = Process.GetProcesses();
+
+            foreach (Process prs in process)
+            {
+                checkedListBox1.Items.Add(prs.ProcessName + "         (" + prs.PrivateMemorySize64.ToString() + prs.Id  + ")");
+            }
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            int index = e.Index;
+            listBox1.Items.Add("Valami");
+            listBox1.Items.Add(process[index].TotalProcessorTime);
+            listBox1.Items.Add(process[index].PagedMemorySize64);
+            listBox1.Items.Add("Valami");
+            //System.Text.StringBuilder messageBoxCS = new System.Text.StringBuilder();
+            //messageBoxCS.AppendFormat("{0} = {1}", "Index", e.Index);
+            //messageBoxCS.AppendLine();
+            //messageBoxCS.AppendFormat("{0} = {1}", "NewValue", e.NewValue);
+            //messageBoxCS.AppendLine();
+            //messageBoxCS.AppendFormat("{0} = {1}", "CurrentValue", e.CurrentValue);
+            //messageBoxCS.AppendLine();
+            //MessageBox.Show(messageBoxCS.ToString(), "ItemCheck Event");
+        }
+
+    }
+}
